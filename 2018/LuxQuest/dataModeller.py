977099b343717a2a997009d8c70773e2cc8efa72
Mark Corkery <mark.corkery@vennetics.com>
2018-02-05 22:14:01 +0000

    Update dataModeller.py
    Updates falling out of the last get together where we'd discussed scaling the graphs to fit inside the minecraft world etc and pulling in location for brightest and darkest images

--- 2018/LuxQuest/dataModeller.py
+++ 2018/LuxQuest/dataModeller.py
@@ -1,34 +1,60 @@
 from mcpi import minecraft
+from mcpi import block
 import csv
 from time import sleep
+import pandas as pd
+import math
+from geopy.geocoders import Nominatim
+import pygame
 
 # Constant values
-tungsten=45
-tnt = 7
-static_lava = 11
-gold_ore = 14
+maxStackHeight = 120
+maxRowLength = 120
 fieldnames = ['time', 'lat', 'long', 'photo', 'lux'] # Same fields we wrote to the file
 resultsDirectory = '/home/pi/luxquestdata/'
-
+geolocator = Nominatim()
+        
 # Creates our minecraft objects and displays start message
 # Returns our initialized minecraft
 def initMineCraft():
     mc = minecraft.Minecraft.create()
-    mc.postToChat("Hello world")
+    ## clear all blocks down to lowest level
+    mc.setBlocks(-128,-64,-128,128,64,128,block.AIR.id)
+    ## set bottom yer to build blocks on
+    mc.setBlocks(-128,-64,-128,128,-64,128,block.SANDSTONE.id)
+    ## move player to corner to start
+    mc.player.setPos(-126, -62, -126)
+    mc.postToChat("Reset world for Lux Quest!")
     return mc
 
 # Based on the lux value, we want to display blocks as different types
 # Returns integer representing block to use
 def blockToUse(lux):
     blockToPlace = 1
-    if lux >= 0 and lux <= 20:
-        blockToPlace = tnt
-    elif lux >= 21 and lux <= 50:
-        blockToPlace = tungsten
-    elif lux >= 256:
-        blockToPlace = static_lava
+    if 0 <= lux <= 10:
+        blockToPlace = block.BRICK_BLOCK.id
+    elif 11 <= lux <= 20:
+        blockToPlace = block.IRON_BLOCK.id
+    elif 21 <= lux <= 30:
+        blockToPlace = block.GOLD_BLOCK.id
+    elif 31 <= lux <= 40:
+        blockToPlace = block.LAPIS_LAZULI_BLOCK.id
+    elif 41 <= lux <= 50:
+        blockToPlace = block.DIAMOND_BLOCK.id
+    elif 51 <= lux <= 60:
+        blockToPlace = block.MELON.id
+    elif 61 <= lux <= 70:
+        blockToPlace = block.GLOWSTONE_BLOCK.id
+    elif 71 <= lux <= 80:
+        blockToPlace = block.CLAY.id
+    elif 81 <= lux <= 90:
+        blockToPlace = block.OBSIDIAN.id
+    elif 91 <= lux <= 100:
+        blockToPlace = block.GLOWING_OBSIDIAN.id
+    elif 101 <= lux <= 110:
+        blockToPlace = block.GLASS_PANE.id
     else:
-        blockToPlace = gold_ore
+        blockToPlace = block.SNOW_BLOCK.id
     return blockToPlace
 
 # Creates our results reader and skips the headers from the file
@@ -38,26 +64,61 @@ def initCsvReader(csvfile):
     next(reader) # this lets us skip the header row
     return reader
 
+# Creates our results reader and skips the headers from the file
+# Returns our initialized reader
+def initPandas(csvfile):
+    df = pd.read_csv(resultsDirectory + 'results.csv')
+    return df
+
+def getLocString(lat, long):
+    location = geolocator.reverse((lat, long))
+    if location.address is None:
+        return "Not over a known location"
+    else:
+        return location.address
+
+# Use PyGame top display the image
+def displayImage(imageLocation, caption):
+    img = pygame.image.load(imageLocation)
+    pygame.display.set_caption(caption)
+    screen = pygame.display.set_mode((640,480))
+    screen.blit(img, (0,0))
+    pygame.display.flip()
+    
 # Main block of code that runs entire program
 def main():
-    mc = initMineCraft()
-    x, y, z = mc.player.getPos()
-        
-    with open(resultsDirectory + 'results.csv') as csvfile:
-        
-        for row in reader:
-            lux = int(float(row['lux'])) # here we are just interested in the lux column
-            mc.postToChat('Lux value: ' + str(lux))
-            mc.setBlocks(x+1, y+1, z+1, x+1, y+lux, z+1, blockToUse(lux))
-            x += 1
-
-            # Things to do:
-            # 1) Add exception handling, i.e. what if results file isn't there
-            # 2) Determine different block types we want to use
-            # 3) Is it better to use players current position or to start somewhere like 0,0,0?
-            # 4) Can we do anything extra with the data, e.g. keep track or brightest and darkest places
-            #    or look up locations of brightest/darkest. Can we open the picture with brightest and
-            #    darkest results?
+    try:
+        mc = initMineCraft()
+    except ConnectionRefusedError:
+        print("Issue connecting to Minecraft! Make sure Minecraft has started!")
+    else:
+        pygame.init()
+        x, y, z = mc.player.getPos()            
+        with open(resultsDirectory + 'results.csv') as csvfile:
+            reader = initCsvReader(csvfile)
+            df = initPandas(csvfile)
+            maxValue = df.ix[df.lux.idxmax(), 'lux']
+            maxLoc = getLocString(df.ix[df.lux.idxmax(), 'lat'], df.ix[df.lux.idxmax(), 'long'])
+            scale = math.ceil(maxValue / maxStackHeight)
+            minValue = df.ix[df.lux.idxmin(), 'lux']
+            minLoc = getLocString(df.ix[df.lux.idxmin(), 'lat'], df.ix[df.lux.idxmin(), 'long'])
+            print("To fit all results, we'll use a scale of " + str(scale))
+            print("Max brightness = " + str(maxValue))
+            print("Min brightness = " + str(minValue))
+            print("Average brightness = " + str(df['lux'].mean()))
+            print("Max brightness recorded at: " + maxLoc)
+            print("Min brightness recorded at: " + minLoc)
+            for row in reader:
+                lux = int(float(row['lux'])) # here we are just interested in the lux column
+                mc.postToChat('Lux value: ' + str(lux))
+                mc.setBlocks(x+1, y+1, z+1, x+1, y+(lux/scale), z+1, blockToUse(lux/scale))
+                x += 1
+                if x >= maxRowLength: # If we get to the end of the world jump
+                    x = -126          # back to the other edge and shift over 
+                    z += 5            # a few blocks before continuing
+            displayImage(df.ix[df.lux.idxmax(), 'photo'], "Brightest Image")
+            sleep(10)
+            pygame.quit()
 
 # Run our awesome code!
 main()
